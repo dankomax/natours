bcf69830a400e3cd0bd61e235bb7c235192123bb
DankoMaksym <dankomaksym@gmail.com>
2020-03-08 19:40:22 +0200

    uncaught exceptions

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', err => {
+  console.log('uncaughtException! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 // console.log(process.env);
@@ -25,8 +31,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('unhandledRejection! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
