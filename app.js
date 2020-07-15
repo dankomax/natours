192e623ce50c1e6b340c5d0ba326f68940943876
DankoMaksym <dankomaksym@gmail.com>
2020-07-16 00:54:09 +0300

    Payments with Stripe

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 // GLOBAL MIDDLEWARES
@@ -93,6 +94,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   // res.status(404).json({
